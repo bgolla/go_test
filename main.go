2a156c8361a6b67ea132c44bde4afda192c8212c
Bhargav Golla <bhargav.golla@siemens.com>
2019-12-02 05:10:11 -0500

    test

--- main.go
+++ main.go
@@ -1,19 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	simplepb "github.com/simplesteph/protobuf-example-go/src/simple"
-	log "github.com/sirupsen/logrus"
-)
+import abc "github.com/bgolla/go_test/simple/abc"
 
 func main() {
-	doSimple()
-}
-func doSimple() {
-	//  a := example_simple.SimpleMessage{}
-	fmt.Printf("this is printf")
-	log.Info("abc")
-	a := simplepb.SimpleMessage{}
-	fmt.Print(a)
+	 abc.testabc()
 }
